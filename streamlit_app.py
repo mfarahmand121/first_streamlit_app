49bb75e6415ff1c581f90475c0ecdc732b327753
Mohsen Farahmand <54422369+mfarahmand121@users.noreply.github.com>
2022-12-26 17:30:38 -0500

    Update streamlit_app.py
    added fruityvice api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
+
+
+#Section for fruityvice api responses
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
