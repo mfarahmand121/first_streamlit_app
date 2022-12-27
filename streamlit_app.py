cbec1dc0d1bac7de8bd77ec92c2c1fbd915323c5
Mohsen Farahmand <Mohsen.Farahmand@basis.net>
2022-12-26 20:01:30 -0500

    function based

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,13 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 streamlit.dataframe(my_fruit_list)
 
 
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
 #Section for fruityvice api responses
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -26,14 +33,13 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 
-except:
+except URLError as e:
     streamlit.error()
 
-    
+
 streamlit.stop()
 
 
